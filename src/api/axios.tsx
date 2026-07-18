88019924f2c36eef1d7b4957cd995e9debb5e3f6
soyab <soyabsstudios@gmail.com>
2026-07-18 14:38:31 +0530

    env

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const apiUrl = import.meta.env.apiL;
+const apiUrl = import.meta.env.VITE_API_URL;
 export const api = axios.create({
   baseURL: apiUrl,
   withCredentials: true,
